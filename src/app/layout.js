14142c6c647f72765601a95e51599b94c1eb01c4
Nikunj <mwnjele@gmail.com>
2026-07-01 22:53:48 +0530

    Split Home page into client and server side and add poetry page (yet to be split)

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,6 @@ import "./globals.css";
 import DottedBackground from "@/components/DottedBackground";
 import ThemeRegistry from "./theme/ThemeRegistry";
 
-export const metadata = {
-  title: "Nidofy",
-  description: "Nikunj Doke's portfolio and poems",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html
