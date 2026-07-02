9f1fa6c71fcfd81dbda20ecda27a669292639747
Nikunj <mwnjele@gmail.com>
2026-07-02 22:37:55 +0530

    Update to pull from .env

--- src/app/data/Api.jsx
+++ src/app/data/Api.jsx
@@ -1,6 +1,6 @@
 // const API_BASE_URL = "/api/";
 // const API_BASE_URL = "http://localhost:8081/";
-const API_BASE_URL = "https://nido.dino.icu/api/";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 async function sendPOSTRequest(url, body, authtoken) {
     try {
